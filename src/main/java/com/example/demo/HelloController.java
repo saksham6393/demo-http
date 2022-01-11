0f047f6e51c5575e91cf93ab353c7653944b32d1
sakshamgupta <saksham.gupta@nagarro.com>
2022-01-12 04:39:43 +0530

    Updated with env variable

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -7,12 +8,16 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class HelloController {
 
+	@Value("${ENV_VAR}")
+	private String envVariable;
+
 	@GetMapping
 	public String hello() {
 		final String uri = "http://nginx-clusterip-service/";
 		RestTemplate restTemplate = new RestTemplate();
 		String result = restTemplate.getForObject(uri, String.class);
 		result = result + "This is from Spring Boot service";
+		result = result + envVariable;
 		return result;
 	}
 
